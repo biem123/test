2b5af88f3640edf734b2ffe357442c0405057e46
biem123 <tuandat8048@gmail.com>
2025-12-19 00:21:08 +0700

    Test deploy tu dong lan 3

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php echo "Hello World! Day la ban deploy tu GitHub Actions"; ?>
+<?php echo  "\nHello World! Day la ban deploy tu GitHub Actions"; ?>
 
-<?php echo "\nHello World! Day la ban deploy tu GitHub Actions lan 2"; ?>
+<?php echo "\nHello World! Day la ban deploy tu GitHub Actions lan 2 \n"; ?>
 
 <?php echo "\nHello World! Day la ban deploy tu GitHub Actions lan 3"; ?>
